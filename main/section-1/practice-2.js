c53e8861a3708e2c0b5a68e20ca0d044a6f71223
Seaborn Lee <seabornlee.cn@gmail.com>
2017-07-28 10:42:06 +0800

    Complete s1p2

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -1,5 +1,21 @@
 'use strict';
 
+function includes(collection, ch) {
+    for (let item of collection) {
+        if (item === ch) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 module.exports = function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+    let result = [];
+    for (let item of collectionA) {
+        if (includes(collectionB[0], item)) {
+            result.push(item);
+        }
+    }
+    return result;
 }
