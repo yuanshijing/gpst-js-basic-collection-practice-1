9ac455e5cd4e5df45b997b542821481c1c9963ea
Seaborn Lee <seabornlee.cn@gmail.com>
2017-07-28 11:45:58 +0800

    Complete s3p3

--- main/section-3/practice-3.js
+++ main/section-3/practice-3.js
@@ -1,5 +1,52 @@
 'use strict';
 
+function find(collection, ch) {
+    for (let item of collection) {
+        if (item.key === ch) {
+            return item;
+        }
+    }
+
+    return null;
+}
+
+function summarize(collection) {
+    let result = [];
+    for (let item of collection) {
+        let obj = find(result, item)
+        if (obj) {
+            obj.count++;
+        } else {
+            result.push({key: item, count: 1});
+        }
+    }
+    return result;
+}
+
+function includes(collection, ch) {
+    for (let item of collection) {
+        if (item === ch) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function discount(collection, promotionItems) {
+    let result = [];
+    for (let item of collection) {
+        let key = item.key;
+        let count = item.count;
+        if (includes(promotionItems, key)) {
+            count = count - Math.floor(count / 3);
+        }
+        result.push({key, count});
+    }
+    return result;
+}
+
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    let summarized = summarize(collectionA);
+    return discount(summarized, objectB.value);
 }
