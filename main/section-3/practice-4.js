20b8c49d8262ec55b4f367b8efd395546cf8d175
Seaborn Lee <seabornlee.cn@gmail.com>
2017-07-28 11:51:44 +0800

    Complete s3p4

--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,5 +1,77 @@
 'use strict';
 
+function find(collection, ch) {
+    for (let item of collection) {
+        if (item.key === ch) {
+            return item;
+        }
+    }
+
+    return null;
+}
+
+function summarize(collection) {
+    let result = [];
+    for (let item of collection) {
+        let obj = find(result, item)
+        if (obj) {
+            obj.count++;
+        } else {
+            result.push({key: item, count: 1});
+        }
+    }
+    return result;
+}
+
+function split(item) {
+    let array = item.split("-");
+    return {key: array[0], count: parseInt(array[1], 10)};
+}
+
+function push(result, key, count) {
+    for (var i = 0; i < count; i++) {
+        result.push(key);
+    }
+}
+
+function expand(collection) {
+    let result = [];
+    for (let item of collection) {
+        if (item.length === 1) {
+            result.push(item);
+        } else {
+            let {key, count} = split(item);
+            push(result, key, count);
+        }
+    }
+    return result;
+}
+
+function includes(collection, ch) {
+    for (let item of collection) {
+        if (item === ch) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function discount(collection, promotionItems) {
+    let result = [];
+    for (let item of collection) {
+        let key = item.key;
+        let count = item.count;
+        if (includes(promotionItems, key)) {
+            count = count - Math.floor(count / 3);
+        }
+        result.push({key, count});
+    }
+    return result;
+}
+
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+    let expandedArray = expand(collectionA);
+    let summarizedArray = summarize(expandedArray);
+    return discount(summarizedArray, objectB.value);
 }
