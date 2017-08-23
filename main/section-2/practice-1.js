061fe989e1c81e3aa299959429c7e45d8a3f7678
yuanshijing <826774822@qq.com>
2017-08-24 01:39:41 +0800

    '2-2'

--- main/section-2/practice-1.js
+++ main/section-2/practice-1.js
@@ -1,23 +1,12 @@
 'use strict';
 
-function find(collection, ch) {
-    let result = null;
-    collection.forEach((i) => {
-        if (i.key === ch)
-            result = i;
-    });
-    return result;
-}
-
 module.exports = function countSameElements(collection) {
-    let result = [];
-    collection.forEach((i) => {
-        let obj = find(result, i)
-        if (obj) {
-            obj.count++;
-        } else {
-            result.push({key: i, count: 1});
-        }
+    let map = new Map();
+    collection.forEach((i) =>{
+        map.set(i,map.has(i)?map.get(i)+1:1);
     })
+    let result = Array.from(map).map(value=>{
+        return {key:value[0],count:value[1]}
+    });
     return result;
 }
--- main/section-2/practice-2.js
+++ main/section-2/practice-2.js
@@ -1,53 +1,14 @@
 'use strict';
 
-function find(collection, ch) {
-    for (let item of collection) {
-        if (item.key === ch) {
-            return item;
-        }
-    }
-
-    return null;
-}
-
-function summarize(collection) {
-    let result = [];
-    for (let item of collection) {
-        let obj = find(result, item)
-        if (obj) {
-            obj.count++;
-        } else {
-            result.push({key: item, count: 1});
-        }
-    }
-    return result;
-}
-
-function split(item) {
-    let array = item.split("-");
-    return {key: array[0], count: parseInt(array[1], 10)};
-}
-
-function push(result, key, count) {
-    for (var i = 0; i < count; i++) {
-        result.push(key);
-    }
-}
-
-function expand(collection) {
-    let result = [];
-    for (let item of collection) {
-        if (item.length === 1) {
-            result.push(item);
-        } else {
-            let {key, count} = split(item);
-            push(result, key, count);
-        }
-    }
-    return result;
-}
-
 module.exports = function countSameElements(collection) {
-    let expandedArray = expand(collection);
-    return summarize(expandedArray);
+    let map = new Map();
+    collection.forEach((i)=>{
+        let [val,cnt]=i.split('-');
+        cnt=cnt?parseInt(cnt):1;
+        map.set(val,map.has(val)?map.get(val)+cnt:cnt);
+    })
+    let result = Array.from(map).map((item)=>{
+        return {key:item[0],count:item[1]}
+    });
+    return result;
 }
